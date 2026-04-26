5ae559bbfcefbad7b7f4194c06830ef3f6079ecd
njomzagashii <njomza.gashi10@student.uni-pr.edu>
2026-04-26 19:53:36 +0200

    Permiresova kodin shtova gjera ne faqe qe japin informacione me shume rreth faqes sone, shtova stilin css qe rregullon pjesen e faqes dhe shtova disa animacione.

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"/>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css"/>
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <title>Royal Hotel</title>
