2e546dbbdf659b6697eb2183aa1ee9cf8cc73de1
Lorenaagaj <lorena.agaj@student.uni-pr.edu>
2026-05-25 17:43:24 +0200

    Create send form

---
+++ pages/Send.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once "../vendor/autoload.php";
+
+if(isset($_POST["send"])){
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'grejonagashi9@gmail.com';
+$mail->Password = 'qzxsjsseiqifclel';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('grejonagashi9@gmail.com', 'Hotel Contact Form');
+$mail->addReplyTo($_POST['email'], $_POST['name']);
+
+$mail->addAddress('grejonagashi9@gmail.com');
+$mail->isHTML(true);
+
+}
