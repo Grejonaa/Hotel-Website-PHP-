a60c220bd8086a7be32fd4a3b68120084b69e6ce
Lorenaagaj <lorena.agaj@student.uni-pr.edu>
2026-05-25 18:25:09 +0200

    update send file

--- pages/Send.php
+++ pages/Send.php
@@ -7,20 +7,33 @@ use PHPMailer\PHPMailer\Exception;
 require_once "../vendor/autoload.php";
 
 if(isset($_POST["send"])){
-$mail = new PHPMailer(true);
+   $mail = new PHPMailer(true);
 
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'grejonagashi9@gmail.com';
-$mail->Password = 'qzxsjsseiqifclel';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
+   $mail->isSMTP();
+   $mail->Host = 'smtp.gmail.com';
+   $mail->SMTPAuth = true;
+   $mail->Username = 'grejonagashi9@gmail.com';
+   $mail->Password = 'qzxsjsseiqifclel';
+   $mail->SMTPSecure = 'ssl';
+   $mail->Port = 465;
 
-$mail->setFrom('grejonagashi9@gmail.com', 'Hotel Contact Form');
-$mail->addReplyTo($_POST['email'], $_POST['name']);
+   $mail->setFrom('grejonagashi9@gmail.com', 'Hotel Contact Form');
+   $mail->addReplyTo($_POST['email'], $_POST['name']);
 
-$mail->addAddress('grejonagashi9@gmail.com');
-$mail->isHTML(true);
+   $mail->addAddress('grejonagashi9@gmail.com');
+   $mail->isHTML(true);
+   $mail->Subject = $_POST["subject"];
+
+   $mail->Body = "<p><strong>Name:</strong> {$_POST['name']}</p>
+               <p><strong>Email:</strong> {$_POST['email']}</p>
+               <p><strong>Phone Number:</strong> {$_POST['phone']}</p>
+               <p><strong>Message:</strong><br>{$_POST['message']}</p>";
+   $mail->send();
+
+   echo "<script>
+         alert('Sent Successfully');
+         document.location.href = 'index.php';
+         </script>";
 
 }
+?>
