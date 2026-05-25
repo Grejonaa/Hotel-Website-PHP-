fbae01e46c7d89094708ef1c4376a195153c0a28
njomzagashii <njomza.gashi10@student.uni-pr.edu>
2026-05-25 15:36:00 +0200

    Added newsletter subscribe system

---
+++ OneDrive/Desktop/web-/Hotel-Website-PHP--main/includes/footer.php
@@ -0,0 +1,53 @@
+<!--Footer-->
+<footer>
+    <div class="container grid">
+        <div class="box">
+            <img src="images/logonobg.png" alt="">
+            <p>Experience unparalleled luxury and timeless elegance at Royal Hotel, where exceptional 
+                service and world-class amenities create unforgettable stays.
+            </p>
+            <div class="icon">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-youtube"></i>
+            </div>
+        </div>
+
+        <div class="box">
+            <h2>Links</h2>
+            <ul>
+                <li>Company History</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Services</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+
+        <div class="box">
+            <h2>Contact Us</h2>
+            <p>
+                Reach us via phone, email, or through our online contact form, and we'll 
+                be delighted to assist you with reservations, special requests, 
+                or any questions you may have. We look forward to welcoming you!
+            </p>
+            <i class="fa fa-location-dot"></i>
+            <label for="">1201 park street, Avenue, Dhanmondy, Dhaka</label><br>
+            <i class="fa fa-phone"></i>
+            <label>[55] 44 576 801</label><br>
+            <i class="fa fa-envelope"></i>
+            <label>royalhotel@gmail.com</label>
+        </div>
+
+    </div>
+</footer>
+<!--Footer-->
+
+<div class="legal">
+    <p class="container">Copyright (c) 2025 Copyright Holder All Rights Reserved</p>
+</div>
+   
+
+</body>
+</html>
