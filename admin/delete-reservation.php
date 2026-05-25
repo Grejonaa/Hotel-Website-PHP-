9af2bfc94c53b9110ae09b635ff9b8684dca8001
melisa829 <melisa.grajcevci@student.uni-pr.edu>
2026-05-25 17:35:02 +0200

    Update delete-reservation.php

---
+++ admin/delete-reservation.php
@@ -0,0 +1,20 @@
+<?php
+include "../config/db.php";
+include "../auth/auth_check.php";
+
+if (!isset($_GET['id'])) {
+    die("ID missing");
+}
+
+$id = intval($_GET['id']);
+
+$stmt = $conn->prepare("DELETE FROM projekt WHERE id=?");
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    header("Location: reservations.php");
+    exit();
+} else {
+    echo "Error deleting reservation.";
+}
+?>
